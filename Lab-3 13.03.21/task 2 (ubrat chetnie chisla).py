e19a9d218f1f8b71b5920e4cf9689a2523f4d216
Artem Smyslov <40802434+KoshakIsus@users.noreply.github.com>
2021-03-15 23:08:24 +0500

    1v

--- Lab-3 13.03.21/task 2 (ubrat chetnie chisla).py
+++ Lab-3 13.03.21/task 2 (ubrat chetnie chisla).py
@@ -1,7 +1,21 @@
 line = input()
 line = line.split()
-#line = map(int, line)
-for i in line:
-	if int(line[i]) % 2 == 0:
-		line.pop([i])
-print(*line, sep=' ')
+
+result = []
+for item in line: #т.к. нужен элемент, а не индекс из списка!!!
+	if int(item) % 2 != 0: #проверка элемента
+		result.append(item) #если нечётный, то добавляем в новый список
+print(*result)
+
+
+#Неверный метод:
+#т.к. здесь брали по индексам, а не по элементам списка!!!
+
+# line = input()
+# line = line.split()
+# #line = map(int, line)
+
+# for i in line:
+# 	if int(line[i]) % 2 == 0:
+# 		line.pop([i])
+# print(*line, sep=' ')
