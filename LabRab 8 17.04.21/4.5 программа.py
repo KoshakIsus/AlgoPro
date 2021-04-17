11b17985486132b16d53d2e8cff54eda38d3deb7
Artem Smyslov <40802434+KoshakIsus@users.noreply.github.com>
2021-04-17 23:34:59 +0500

    .

--- LabRab 8 17.04.21/4.5 программа.py
+++ LabRab 8 17.04.21/4.5 программа.py
@@ -10,7 +10,7 @@ conn = pymysql.connect(
 
 cur = conn.cursor()
 
-sql = ("SELECT `nameStud`, `rating`, `city` FROM `Students` ORDER BY `city`) ORDER BY `rating` DESC")
+sql = "SELECT `nameStud`, `rating`, `city` FROM `Students` ORDER BY `city`) ORDER BY `rating` DESC"
 cur.execute(sql)
 rows = cur.fetchall()
 
